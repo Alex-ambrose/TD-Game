a6a4f41388d7095fab3529f2e2eb5c0d6e331130
Alex-ambrose <115847143+Alex-ambrose@users.noreply.github.com>
2025-10-04 21:22:22 -0500

    updated ProjectileController

--- Assets/Scrips/ProjectileController.cs
+++ Assets/Scrips/ProjectileController.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    
-    
+    public EnemyController myTarget;
     //todo move me towards the target 
     //todo onclolide deal damage
     void Update()
     {
-        
+        var movementDesired = myTarget.transform.position - transform.position;
+        if (movementDesired.magnitude < 0.1f)
+        {
+            movementDesired = myTarget.transform.position - transform.position;
+
+        }
+        transform.position += movementDesired.normalized * Time.deltaTime * 15;
     }
     public void SetTarget(EnemyController enemy)
     {
-        var movementDesired = enemy.transform.position - transform.position;
-        if (movementDesired.magnitude < 0.1f)
-        {   
-            movementDesired = enemy.transform.position - transform.position;
-        }
-        transform.position += movementDesired.normalized * Time.deltaTime * 15;
+        myTarget = enemy;
     }
 }
 
